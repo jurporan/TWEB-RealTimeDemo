690030907e9f53e664c90ac99f2813b1dee0e034
jurporan <jan.purro@heig-vd.ch>
2015-11-19 14:12:07 +0100

    Deploying to heroku

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
-
 var express = require('express'),
   config = require('./config/config');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 require('./config/express')(app, config);
 
@@ -11,3 +11,18 @@ app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
 
+io.on('connection', function(socket)
+{
+    socket.emit('news', { hello: 'world'});
+    socket.on('my other event', function (data)
+    {
+        console.log(data);
+    });
+});
+
+var socket = io.connect('http://localhost');
+socket.on('news', function
+{
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+});
